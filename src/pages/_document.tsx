ab00ca9bad5ed3842c1a73b46b6cf2939e29a307
MatthewBloemke <mattbloemke@gmail.com>
2025-10-02 13:07:15 -0500

    added favicon and updated packages

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,20 @@ export default function Document(props: any) {
     <Html lang="en">
       <Head>
         <DocumentHeadTags {...props} />
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </Head>
       <body>
         <Main />
